37c1b91147ad0fd7212e3fd1356ac087cadcfd97
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-01-15 10:37:43 +0100

    added error handlers

--- src/server.js
+++ src/server.js
@@ -1,30 +1,43 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const helmet = require("helmet")
-const endpointsList = require("express-list-endpoints")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const endpointsList = require("express-list-endpoints");
 
-const server = express()
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
+const {
+  notFoundErrorHandler,
+  unauthorizedErrorHandler,
+  badRequestErrorHandler,
+  forbiddenErrorHandler,
+  catchAllErrorHandler,
+} = require("./errorHandlers");
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
 
-port = process.env.PORT || 3002
+server.use(badRequestErrorHandler);
+server.use(notFoundErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(unauthorizedErrorHandler);
+server.use(catchAllErrorHandler);
 
-console.log(endpointsList(server))
+port = process.env.PORT || 3002;
+
+console.log(endpointsList(server));
 
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_LOCAL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
+    });
     server.listen(port, () => {
-      console.log("server running on port", port)
-    })
+      console.log("server running on port", port);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-connectDb()
+connectDb();
