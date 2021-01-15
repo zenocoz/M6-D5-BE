8f3ff49606e26cb7b0229736fb382928836e0580
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-01-15 12:21:19 +0100

    finished the CRUD for products

---
+++ src/services/products/schema.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const ProductSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    brand: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: String,
+  },
+  { timestamps: true }
+);
+
+const ProductModel = model("Product", ProductSchema);
+
+module.exports = ProductModel;
