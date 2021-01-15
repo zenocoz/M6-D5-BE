9e4e7eaaf8be0efabbb1a7d1a0a06b7d92bb4b69
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-01-15 16:56:38 +0100

    almost finished

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const ProductModel = require("../products/schema")
+const express = require("express");
+const mongoose = require("mongoose");
+const ProductModel = require("../products/schema");
 
-const UsersModel = require("./schema")
+const UsersModel = require("./schema");
 
-const usersRouter = express.Router()
+const usersRouter = express.Router();
 
 // usersRouter.post("/", async (req, res, next) => {
 //   try {
@@ -21,84 +21,99 @@ usersRouter.get("/:cartId", async (req, res, next) => {
   try {
     const cart = await UsersModel.findById(
       req.params.cartId
-    ).populate("products", { reviews: 0 })
+    ).populate("products", { reviews: 0 });
     if (cart) {
-      res.status(200).send(cart)
+      res.status(200).send(cart);
     } else {
-      const err = new Error()
-      err.message = `Cart id: ${req.params.cartId} not found!`
-      err.httpStatusCode = 404
-      next(err)
+      const err = new Error();
+      err.message = `Cart id: ${req.params.cartId} not found!`;
+      err.httpStatusCode = 404;
+      next(err);
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 usersRouter.post("/:cartId/carts/:productId", async (req, res, next) => {
   try {
     const product = await ProductModel.decreaseProductQuantity(
       req.params.productId,
       req.body.quantity
-    )
+    );
     if (product) {
-      const newProduct = { ...product.toObject(), quantity: req.body.quantity }
+      const newProduct = { ...product.toObject(), quantity: req.body.quantity };
       const isProductThere = await UsersModel.findProduct(
         req.params.cartId,
         req.params.productId
-      )
+      );
 
       if (isProductThere) {
         await UsersModel.incrementCartQuantity(
           req.params.cartId,
           req.params.productId,
           req.body.quantity
-        )
-        res.send("quantity incremented")
+        );
+        res.send("quantity incremented");
       } else {
-        const cart = await UsersModel.findByIdAndUpdate(
-          req.params.cartId,
-          {
-            $addToSet: {
-              products: mongoose.Types.ObjectId(req.params.productId),
-            },
-          },
-          { runValidators: true, new: true }
-        )
+        const cart = await UsersModel.addToCart(req.params.cartId, newProduct);
+
         if (cart) {
-          res.status(201).send("product added to cart")
+          res.status(201).send("product added to cart");
         } else {
-          const err = new Error()
-          err.message = `Cart id: ${req.params.cartId} not found!`
-          err.httpStatusCode = 404
-          next(err)
+          const err = new Error();
+          err.message = `Cart id: ${req.params.cartId} not found!`;
+          err.httpStatusCode = 404;
+          next(err);
         }
       }
     } else {
-      const err = new Error()
-      err.message = "product not found"
-      err.httpStatusCode = 404
-      next(err)
+      const err = new Error();
+      err.message = "product not found";
+      err.httpStatusCode = 404;
+      next(err);
     }
   } catch (error) {
-    console.log(error), next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 usersRouter.delete("/:cartId/carts/:productId", async (req, res, next) => {
   try {
-    const cart = await UsersModel.findByIdAndUpdate(
-      req.params.cartId,
-      {
-        $pull: { products: mongoose.Types.ObjectId(req.params.productId) },
+    const { products } = await UsersModel.findById(req.params.cartId, {
+      _id: 0,
+      products: {
+        $elemMatch: { _id: mongoose.Types.ObjectId(req.params.productId) },
       },
-      { runValidators: true, new: true }
-    )
-    res.send("product deleted")
-  } catch (error) {
-    console.log(error)
-  }
-})
+    });
 
-module.exports = usersRouter
+    await ProductModel.increaseProductQuantiy(req.productId, req.body.quantity);
+
+    const product = 0;
+    console.log(product);
+    if (product === 0) {
+      await UsersModel.findByIdAndUpdate(
+        req.params.cartId,
+        {
+          $pull: { products: mongoose.Types.ObjectId(req.params.productId) },
+        },
+        { runValidators: true, new: true }
+      );
+      res.status(203).send("deleted");
+    } else {
+      await UsersModel.decreaseCartQuantity(
+        req.params.cartId,
+        req.params.productId,
+        req.body.quantity
+      );
+      res.status(203).send("product deleted");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+module.exports = usersRouter;
--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -4,8 +4,60 @@ const UserSchema = new Schema({
   ownerId: { type: String, required: true },
   name: { type: String, required: true },
   surname: { type: String, required: true },
-  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+  products: [
+    {
+      _id: { type: Schema.Types.ObjectId, ref: "Product" },
+      quantity: Number,
+    },
+  ],
   total: Number,
+});
+
+UserSchema.static("findProduct", async function (id, productId) {
+  const isProductThere = await UserModel.findOne({
+    _id: id,
+    "products._id": productId,
+  });
+  return isProductThere;
+});
+
+UserSchema.static(
+  "incrementCartQuantity",
+  async function (id, productId, quantity) {
+    await UserModel.findOneAndUpdate(
+      {
+        _id: id,
+        "products._id": productId,
+      },
+      {
+        $inc: { "products.$.quantity": quantity },
+      }
+    );
+  }
+);
+
+UserSchema.static(
+  "decreaseCartQuantity",
+  async function (id, productId, quantity) {
+    await UserModel.findOneAndUpdate(
+      {
+        _id: id,
+        "products._id": productId,
+      },
+      {
+        $inc: { "products.$.quantity": -quantity },
+      }
+    );
+  }
+);
+
+UserSchema.static("addToCart", async function (id, product) {
+  const productToAdd = await UserModel.findByIdAndUpdate(id, {
+    $addToSet: {
+      products: product,
+    },
+  });
+  return productToAdd;
 });
 
 const UserModel = model("User", UserSchema);
